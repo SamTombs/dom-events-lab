0c98f11571db21c120c22498c3868e8908cc330a
SamTombs <samuel.tombs91@outlook.com>
2025-04-22 18:21:50 +0200

    DOM Lab

--- app.js
+++ app.js
@@ -8,66 +8,91 @@
 
 /*-------------------------------- Functions --------------------------------*/
 
-// As a user, I want to be able to select numbers so that I can perform operations with them.
+const allNumbers = document.querySelectorAll('.number');
+console.log(allNumbers);
+const operatorButtons = document.querySelectorAll('.operator');
+console.log(operatorButtons);
+const equalsButton = document.querySelectorAll('.equals');
+console.log(equalsButton);
 
-const allButtons = document.querySelectorAll('.button');
-const isFirstNumber = true;
-// two numbers are set to null as they havent taken a value yet
+// two numbers and operator are set to null as they havent taken a value yet
 let firstNumber = null;
 let secondNumber = null;
-const add = document.querySelector('.button operator add')
-const equals = document.querySelector(".button equals");
+let operator = null;
+
+console.log(firstNumber, secondNumber, operator);
+
 
 function logWhichButton(event) {
     console.log(event);
-    //what is event.target
     const theElement = event.target;
     console.log(theElement);
 
-    // learn what this is parse INT?
-    if(firstNumber === null) {
+    // Found a way to search for a string which then can pass in a number or operator
+    if(firstNumber === null && theElement.classList.contains('number')) {
         firstNumber = parseInt(event.target.textContent);
+        //console logs number
+        console.log(firstNumber);
         return;
-    }
-//what is event.target?? They are the elements (button or so)
-    secondNumber = parseInt(event.target.textContent);
 
-}
+    } else if (firstNumber !== null && theElement.classList.contains('number')) {
+        secondNumber = parseInt(event.target.textContent);
+        console.log(secondNumber);
+        return;
 
-// As a user, I want to be able to add two numbers together.
+    } else if (theElement.classList.contains('operator')) {
+        operator = event.target.textContent;
+        console.log(operator);
+        return;
+    } else if (theElement.classList.contains("equals")) {
+        const calculation = doCalculation(firstNumber, secondNumber, operator);
+        console.log(calculation);
+          }
+        
+};
+// Now whenever I select a button it will console log what I have clicked combined with the below
 
 // this will look for all buttons and then add an event listener when clicked of logWhich Button function
-allButtons.forEach((button) => {
-    button.addEventListener('click', logWhichButton) 
+// clicking a button does nothing without this
+
+allNumbers.forEach((button) => {
+    button.addEventListener('click', logWhichButton)
+    console.log(allNumbers); 
     });
 
-    // this function is where we are actually going to add 
-    add.addEventListener('click', () => {
-        console.log('you are trying to add');
-        if (firstNumber && secondNumber) {
-            console.log(firstNumber + secondNumber);
-        } else {
-            console.log("not enough numbers");
-        }
+operatorButtons.forEach((operator) => {
+    operator.addEventListener('click', logWhichButton)
+    console.log(operatorButtons); 
     });
 
+equalsButton.forEach((equals) => {
+    equals.addEventListener('click', logWhichButton)
+    console.log(equalsButton); 
+    });
 
+// need a new function now to perform the calculation
 
+function doCalculation(firstNum, secondNum, operator) {
+    if (operator === "+") {
+      return firstNum + secondNum;
+    } else if (operator === "-") {
+        return firstNum - secondNum;
+    } else if (operator === "*") {
+        return firstNum * secondNum;
+    } else if (operator === "/") {
+        return firstNum / secondNum;
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-// As a user, I want to be able to subtract one number from another.
-// As a user, I want to be able to multiply two numbers together.
-// As a user, I want to be able to divide one number by another.
 // As a user, I want to be able to see the output of the mathematical operation.
+
+
+
+
+
+
+
+
+
+
 // As a user, I want to be able to clear all operations and start from 0.
