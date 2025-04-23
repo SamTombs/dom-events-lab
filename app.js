cca4c09a227e7c8656738ed520eabb9e782dd011
SamTombs <samuel.tombs91@outlook.com>
2025-04-23 20:26:22 +0200

    Finally finished

--- app.js
+++ app.js
@@ -12,8 +12,12 @@ const allNumbers = document.querySelectorAll('.number');
 console.log(allNumbers);
 const operatorButtons = document.querySelectorAll('.operator');
 console.log(operatorButtons);
+const clearCalculation = document.querySelectorAll('.clear');
+console.log(clearCalculation);
 const equalsButton = document.querySelectorAll('.equals');
 console.log(equalsButton);
+const displayResultElement = document.querySelector('.display');
+console.log(displayResultElement)
 
 // two numbers and operator are set to null as they havent taken a value yet
 let firstNumber = null;
@@ -28,6 +32,8 @@ function logWhichButton(event) {
     const theElement = event.target;
     console.log(theElement);
 
+    //Informed a best practice way is to introduce isCalculating with a boolean, so it will check if a calculation has been made
+    let isCalculating = false;
     // Found a way to search for a string which then can pass in a number or operator
     if(firstNumber === null && theElement.classList.contains('number')) {
         firstNumber = parseInt(event.target.textContent);
@@ -44,11 +50,24 @@ function logWhichButton(event) {
         operator = event.target.textContent;
         console.log(operator);
         return;
+
     } else if (theElement.classList.contains("equals")) {
-        const calculation = doCalculation(firstNumber, secondNumber, operator);
-        console.log(calculation);
-          }
-        
+        if (!isCalculating) {
+        const calculationResult = doCalculation(firstNumber, secondNumber, operator);
+        console.log(calculationResult);
+        displayResultElement.textContent = calculationResult;
+        isCalculating === true;
+        }
+
+     } else if (theElement.classList.contains('clear')) {
+        //my calculations after clear were not correctly calculating. Put all values to null after clear to fix
+        firstNumber = null
+        secondNumber = null
+        operator = null
+        displayResultElement.textContent = 0;
+        isCalculating = false;
+     }
+   
 };
 // Now whenever I select a button it will console log what I have clicked combined with the below
 
@@ -70,7 +89,13 @@ equalsButton.forEach((equals) => {
     console.log(equalsButton); 
     });
 
-// need a new function now to perform the calculation
+clearCalculation.forEach((clear) => {
+    clear.addEventListener('click', logWhichButton)
+    console.log(clearCalculation); 
+    });
+
+// Struggled for a long time trying to find a function, wasn't aware of do calculation, thank you Alara!
+
 
 function doCalculation(firstNum, secondNum, operator) {
     if (operator === "+") {
@@ -84,15 +109,7 @@ function doCalculation(firstNum, secondNum, operator) {
     }
 }
 
+
 // As a user, I want to be able to see the output of the mathematical operation.
 
-
-
-
-
-
-
-
-
-
-// As a user, I want to be able to clear all operations and start from 0.
+// Overall, struggled with this lab and had to learn a lot of new things. Hopefully the knowledge will improve over the rest of the course.
